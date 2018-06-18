173b09e99fe316c0cb898d872df9bd71801ea4e4
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-06-18 12:38:38 +0530

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './components/site-header/site-header.component';
+import { SiteFooterComponent } from './components/site-footer/site-footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
