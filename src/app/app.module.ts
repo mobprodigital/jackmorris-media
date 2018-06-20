8959cfdb133c73cac39c940cf54f802df15bf087
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-06-20 10:54:15 +0530

    ng bootstrap module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
 import { SiteFooterComponent } from './components/site-footer/site-footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteHeaderComponent,
-    SiteFooterComponent
+    SiteFooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
