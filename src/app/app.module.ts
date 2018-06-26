71c7fed465d85f0340e58f9dad2d9cebe5ac38f1
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-06-26 10:42:54 +0530

    about us pahe html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,21 @@ import { AppComponent } from './app.component';
 import { SiteHeaderComponent } from './components/site-header/site-header.component';
 import { SiteFooterComponent } from './components/site-footer/site-footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+import { Routes, RouterModule } from '@angular/router';
 
 // bootstrap module
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AboutUsComponent } from './components/about-us/about-us.component'
+
+const appRoutes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'home',      component: HomeComponent },
+  { path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +31,8 @@ import { AboutUsComponent } from './components/about-us/about-us.component'
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
