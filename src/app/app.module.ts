5c03e899b220b4d2b91f36ed16a40859f06dee9f
Gyan Parkash <gyan.p@morrisdigital.mobi>
2018-06-26 17:13:14 +0530

    contact us page html added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,19 @@ import { SiteFooterComponent } from './components/site-footer/site-footer.compon
 import { HomeComponent } from './components/home/home.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { OurServicesComponent } from './components/our-services/our-services.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
+// routing
 import { Routes, RouterModule } from '@angular/router';
 
 // bootstrap module
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 const appRoutes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
   { path: 'home', component: HomeComponent },
   { path: 'solutions', component: OurServicesComponent },
+  { path: 'contact-us', component: ContactUsComponent },
   {
     path: '',
     redirectTo: 'home',
